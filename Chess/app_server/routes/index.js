a41de4bf328e6e32f92e3a6771058c01b1b5a4e5
Phelps Merrell <starfish206@gmail.com>
2019-09-20 22:41:49 -0500

    second commit

--- Chess/app_server/routes/index.js
+++ Chess/app_server/routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+ 
   res.render('index', { title: 'Express' });
 });
+
+/*GET chess Page */
+router.get('/chess/', (req,res,next) =>{
+  var board1 = Chessboard('board1', 'start');
+  res.render('color',{
+    title: 'Chess Page',
+    board1: board1
+  });
+
+});
+
+
 
 module.exports = router;
